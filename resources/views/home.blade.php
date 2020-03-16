dbc05bcf6082ca064acd5007c560a682e1c47c25
vincenzovolpe <vinci792010@gmail.com>
2020-03-16 10:09:18 +0100

    Sistemato conflitto su home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -35,24 +35,6 @@
         <h2 class="text-center">Appartamenti in primo piano</h2>
             <!-- Card package -->
         <div id="sponsored" class="card-columns">
-<<<<<<< HEAD
-            @forelse ($flat_sponsored as $flat)
-                <div class="card">
-                    <img class="card-img" src="{{asset('storage/' . $flat->img_uri)}}" alt="">
-                    <div class="card-img-overlay">
-                        <a href="#" class="btn btn-light btn-sm"></a>
-                    </div>
-                    <div class="card-body">
-                        <h4 class="card-title">{{ $flat->title }}</h4>
-                        {{-- <p class="card-text">Propietario: {{$flat->user->name}}</p> --}}
-                        <a class="btn btn-danger stretched-link" style="position: relative;" href="{{ route('flat.details', $flat->id)}}">Vedi Dettagli</a>
-                    </div>
-                    <div class="card-footer text-muted d-flex justify-content-between bg-transparent border-top-0">
-                        <div class="views">{{ $flat->updated_at }}
-                        </div>
-                        <div class="stats">
-                            {{ $flat->view }}
-=======
             @if(isset($flat_sponsored) && $flat_sponsored->count())
                 @foreach ($flat_sponsored as $flat)
                     <div class="card">
@@ -71,12 +53,11 @@
                             <div class="stats">
                                 {{$flat->view}}
                             </div>
->>>>>>> origin/master
                         </div>
                     </div>
                 @endforeach
             @else
-                Non ci sono appartamenti sponsorizzati
+                <p>Non ci sono appartamenti sponsorizzati</p>
             @endif
         </div>
     </br>
@@ -103,7 +84,7 @@
                     </div>
                 </div>
             @empty
-                Non ci sono appartamenti
+                <p>Non ci sono appartamenti</p>
             @endforelse
         </div>
         <!-- Card package -->
