a1701764c602a7ccecd0df774185863fd136b5c5
Aldo-cav <aldocav@live.com>
2020-03-19 09:17:16 +0100

    logo in navbar e altri dettagli

--- resources/views/layouts/upr.blade.php
+++ resources/views/layouts/upr.blade.php
@@ -18,6 +18,12 @@
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+
+    <!-- favicon -->
+    <link rel="apple-touch-icon" sizes="180x180" href="/assets/images/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="/assets/images/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="/assets/images/favicon-16x16.png">
+
 </head>
 <body>
     <div id="app">
