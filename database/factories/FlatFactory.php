6a68682a1dd21be3f4edfb3540a7e4e94025f6f3
UmbertoV <umberto.ottone@gmail.com>
2020-03-21 14:51:56 +0100

    Modifica dei Flat faker

--- database/factories/FlatFactory.php
+++ database/factories/FlatFactory.php
@@ -9,15 +9,15 @@ use Faker\Generator as Faker;
 
 $factory->define(Flat::class, function (Faker $faker) {
     return [
-        'title' => $faker->city,
+        'title' =>('Appartamento in ').$faker->country,
         'room_qty' => $faker->numberBetween($min = 1, $max = 5) ,
         'bed_qty' => $faker->numberBetween($min = 1, $max = 10) ,
-        'bath_qty' => $faker->numberBetween($min = 1, $max = 2) ,
-        'sq_meters' => $faker->numberBetween($min = 60, $max = 200) ,
+        'bath_qty' => $faker->numberBetween($min = 1, $max = 5) ,
+        'sq_meters' => $faker->numberBetween($min = 10, $max = 500) ,
         'address' => $faker->streetAddress,
         'lat' => $faker->latitude($min = -90, $max = 90),
         'lon' => $faker->longitude($min = -180, $max = 180),
-        'img_uri' => ' ',
+        'img_uri' => $faker->imageUrl($width = 640, $height = 480),
         'active' => '1',
     ];
 });
