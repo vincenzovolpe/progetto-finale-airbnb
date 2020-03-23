3a706f5ff0ff45593b3e31f98575e50f951ecd57
vincenzovolpe <vinci792010@gmail.com>
2020-03-23 18:44:41 +0100

    Installazione Braintree pagamenti e collaudo per sponsorizzare gli appartamenti

--- resources/views/upr/flats/sponsor.blade.php
+++ resources/views/upr/flats/sponsor.blade.php
@@ -13,22 +13,17 @@
             <a class="btn btn-info" href="{{ route('upr.flats.index') }}">Back to index!</a>
         </div>
         <div class="col">
-            <form method="POST" action="{{ route('upr.flats.submit') }}" enctype="multipart/form-data">
-                @csrf
-                @method("POST")
+            <form>
                 <!-- Inserire qui una lista delle opzioni di sponsorizzazione: -->
                 @forelse($sponsor as $single_sponsor)
                     <input type="hidden" id="{{ $flat->id }}" name="flat_id" value="{{ $flat->id }}" required>
                     <input type="radio" id="{{ $single_sponsor->id }}" name="sponsor_id" value="{{ $single_sponsor->id }}" required>
                     <label for="{{ $single_sponsor->id }}">Sponsorizza per <strong>{{ $single_sponsor->hours }}</strong> ore, alla modica cifra di:<strong>{{ $single_sponsor->price }}€</strong></label>!<br>
+                    <a href="#"></a>
+                    <button type="button" name="button"><a href="{{ route('upr.payment.index', ['id' => $flat->id, 'sponsor_id' => $single_sponsor->id]) }}">Effettua il pagamento</a></button>
                 @empty
                     <p>Non abbiamo nessun servizio di sponsorizzazione attivo al momento! :(</p>
                 @endforelse
-                {{-- Se esistono sponsor nel DB, mostra il pulsante di submit: --}}
-                @if($sponsor->count())
-                    <input type="submit" class="btn btn-success" value="Sponsorizza!">
-                    {{-- Dobbiamo verificare che l'appartamento non abbia già sponsorizzazioni, prima di procedere!! --}}
-                @endif
             </form>
         </div>
     </div>
