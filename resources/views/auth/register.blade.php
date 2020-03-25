55ff66e6f5a1524611df6b68f9ee962c6d7b2718
vincenzovolpe <vinci792010@gmail.com>
2020-03-25 09:47:19 +0100

    Popup conferma cancellazione appartamento

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -2,107 +2,91 @@
 
 @section('login-content')
 <div class="container">
-    <div id="homeLeftBox" class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Register') }}</div>
-
-                <div class="card-body">
-                    <form class="needs-validation" novalidate method="POST" action="{{ route('register') }}">
-                        @csrf
-
-                        <!-- Aggiunto campo inserimento nome -->
-                        <div class="form-group row">
-                            <label for="name" class="col-md-4 col-form-label text-md-right">{{ __('Name') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="name" type="text" minlength="3" maxlength="20" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus style="text-transform:capitalize">
-                                <div class="name valid-feedback">
-                                    Inserimento corretto!
-                                </div>
-                                <div class="name invalid-feedback">
-                                    Inserisci un nome valido - min 3 caratteri
-                                </div>
+    <div id="homeLeftBox" class="row">
+        <div class="col-lg-6 card border-light shadow">
+            <div>
+                <h3>{{ __('Register') }}</h3>
+            </div>
+            <div class="card-body">
+                <form class="needs-validation" novalidate method="POST" action="{{ route('register') }}">
+                    @csrf
+                    <!-- Aggiunto campo inserimento nome -->
+                    <div class="form-group row">
+                        <label for="name" class="col-md-4 col-form-label text-md-right">{{ __('Name') }}</label>
+                        <div class="col-md-6">
+                            <input id="name" type="text" minlength="3" maxlength="20" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus style="text-transform:capitalize">
+                            <div class="name valid-feedback">
+                                Inserimento corretto!
+                            </div>
+                            <div class="name invalid-feedback">
+                                Inserisci un nome valido - min 3 caratteri
                             </div>
                         </div>
-
-                        <!-- Aggiunto campo inserimento cognome -->
-                        <div class="form-group row">
-                            <label for="surname" class="col-md-4 col-form-label text-md-right">{{ __('Surname') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="surname" type="text" minlength="3" maxlength="20" class="form-control @error('surname') is-invalid @enderror" name="surname" value="{{ old('surname') }}" required autocomplete="surname" autofocus style="text-transform: capitalize">
-                                <div class="surname valid-feedback">
-                                    Inserimento corretto!
-                                </div>
-                                <div class="surname invalid-feedback">
-                                    Inserisci un nome valido - min 3 caratteri
-                                </div>
-
+                    </div>
+                    <!-- Aggiunto campo inserimento cognome -->
+                    <div class="form-group row">
+                        <label for="surname" class="col-md-4 col-form-label text-md-right">{{ __('Surname') }}</label>
+                        <div class="col-md-6">
+                            <input id="surname" type="text" minlength="3" maxlength="20" class="form-control @error('surname') is-invalid @enderror" name="surname" value="{{ old('surname') }}" required autocomplete="surname" autofocus style="text-transform: capitalize">
+                            <div class="surname valid-feedback">
+                                Inserimento corretto!
+                            </div>
+                            <div class="surname invalid-feedback">
+                                Inserisci un nome valido - min 3 caratteri
                             </div>
                         </div>
-
-                        <!-- Aggiunto campo inserimento data di nascita -->
-                        <div class="form-group row">
-                            <label for="date_of_birth" class="col-md-4 col-form-label text-md-right">{{ __('Date of birth') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="date_of_birth" type="date" class="form-control @error('date_of_birth') is-invalid @enderror" name="date_of_birth" value="{{ old('date_of_birth') }}" required autocomplete="date_of_birth" autofocus>
-                                <div class="date valid-feedback">
-                                    Inserimento corretto!
-                                </div>
-                                <div class="date invalid-feedback">
-                                    Devi avere almeno 18 anni
-                                </div>
+                    </div>
+                    <!-- Aggiunto campo inserimento data di nascita -->
+                    <div class="form-group row">
+                        <label for="date_of_birth" class="col-md-4 col-form-label text-md-right">{{ __('Date of birth') }}</label>
+                        <div class="col-md-6">
+                            <input id="date_of_birth" type="date" class="form-control @error('date_of_birth') is-invalid @enderror" name="date_of_birth" value="{{ old('date_of_birth') }}" required autocomplete="date_of_birth" autofocus>
+                            <div class="date valid-feedback">
+                                Inserimento corretto!
+                            </div>
+                            <div class="date invalid-feedback">
+                                Devi avere almeno 18 anni
                             </div>
                         </div>
-
-                        <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email"  {{-- onclick="validateEmail('#email')"--}} >
-                                <div class="mail valid-feedback">
-                                    Inserimento corretto!
-                                </div>
-                                <div class="mail invalid-feedback">
-                                    Inserisci una mail corretta
-                                </div>
+                    </div>
+                    <div class="form-group row">
+                        <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
+                        <div class="col-md-6">
+                            <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email"  {{-- onclick="validateEmail('#email')"--}} >
+                            <div class="mail valid-feedback">
+                                Inserimento corretto!
+                            </div>
+                            <div class="mail invalid-feedback">
+                                Inserisci una mail corretta
                             </div>
                         </div>
-
-                        <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" minlength="8" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
-                                <div class="valid-feedback">
-                                    Inserimento corretto!
-                                </div>
-                                <div class="invalid-feedback">
-                                    Inserisci una password valida
-                                </div>
+                    </div>
+                    <div class="form-group row">
+                        <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
+                        <div class="col-md-6">
+                            <input id="password" type="password" minlength="8" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
+                            <div class="valid-feedback">
+                                Inserimento corretto!
+                            </div>
+                            <div class="invalid-feedback">
+                                Inserisci una password valida
                             </div>
                         </div>
-
-                        <div class="form-group row">
-                            <label for="password-confirm" class="col-md-4 col-form-label text-md-right">{{ __('Confirm Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password-confirm" type="password" class="form-control @error('password_confirmation') is-invalid @enderror" name="password_confirmation" required autocomplete="new-password">
-
-                            </div>
+                    </div>
+                    <div class="form-group row">
+                        <label for="password-confirm" class="col-md-4 col-form-label text-md-right">{{ __('Confirm Password') }}</label>
+                        <div class="col-md-6">
+                            <input id="password-confirm" type="password" class="form-control @error('password_confirmation') is-invalid @enderror" name="password_confirmation" required autocomplete="new-password">
                         </div>
-
-                        <div class="form-group row mb-0">
-                            <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Register') }}
-                                </button>
-                            </div>
+                    </div>
+                    <div class="form-group row mb-0">
+                        <div class="col-md-6 offset-md-4">
+                            <button type="submit" class="btn btn-primary">
+                                {{ __('Register') }}
+                            </button>
                         </div>
-                    </form>
-                </div>
+                    </div>
+                </form>
             </div>
         </div>
     </div>
--- resources/views/find_flat.blade.php
+++ resources/views/find_flat.blade.php
@@ -2,40 +2,39 @@
 
 @section('content')
     <div class="container">
-        <div class="row">
-            <div class="col-12">
-                <div id="flat_search">
-                    <h2 class="text-left">Ricerca avanzata</h2>
-                    <form id="form_find">
-                        <div class="form-row">
-                            <div class="form-group col-md-6">
-                                <div class="fuzzy-find form-group">
-                                </div>
+        <div class="row col-md-6">
+            <div id="flat_search" class="card">
+                <h2 class="card-body">Ricerca avanzata</h2>
+                <form id="form_find">
+                    <div class="col">
+                        <div class="form-group">
+                            <div class="fuzzy-find form-group">
                             </div>
                         </div>
                         <h4 class="text-left">Filtra i risultati</h4>
                         <div class="form-row">
-                            <div class="form-group col-md-2">
+                            <div class="form-group col-md-4">
                                 <!-- Inserimento numero di stanze -->
                                 <label for="room_qty" class="text-md-right">Numero stanze</label>
                                 <input id="room_qty" type="number" name="room_qty" class="form-control">
                             </div>
-                            <div class="form-group col-md-2">
+                            <div class="form-group col-md-4">
                                 <!-- Inserimento numero di letti -->
                                 <label for="bed_qty" class="text-md-right">Numero Letti</label>
                                 <input id="bed_qty" type="number" name="bed_qty" class="form-control">
                             </div>
-                            <div class="form-group col-md-2">
+                            <div class="form-group col-md-4">
                                 <!-- Inserimento distanza in km -->
                                 <label for="km_radius" class="text-md-right">Raggio di ricerca</label>
                                 <input id="km_radius" type="range" name="km_radius" min="20" max="100" value="20" onchange="distance.value = this.value" class="form-control">
                                 <output id="distance"></output>Km<br>
                             </div>
                         </div>
-
+                    </div>
+                    <div class="col-md-6">
+                        <h5>Servizi disponibili</h5> <br>
                         <div class="form-group form-check col-md-6">
                             <!-- Servizi Aggiuntivi -->
-                            <label>Servizi disponibili</label> <br>
                             @foreach ($services as $service)
                             <input type="checkbox" id="{{$service->id}}" class="form-check-input" name="check_services" value="{{$service->id}}">
                             <label class="form-check-label" for="{{$service->name}}"></label>{{$service->name}}<br>
@@ -46,23 +45,22 @@
                         <input id="latNumberFind" type="text" name="lat" value="{{ $lat }}" hidden>
                         <input id="lonNumberFind" type="text" name="lon" value="{{ $lon }}" hidden>
                         <div class="form-group col-md-6">
-                            <button id="btn_find" class="btn btn-success btn-lg float-right" type="submit" name="btn_find">Cerca</button>
+                            <button id="btn_find" class="btn btn-success btn-lg" type="submit" name="btn_find">Cerca</button>
                             <br>
                         </div>
-                    </form>
-                </div>
+                    </div>
+                </form>
             </div>
         </div>
-        <div class="row">
+        <div id="find-results" class="row">
             <!-- Card package -->
             <div id="card_container" class="card-columns">
-                {{-- Appendo il contenuto del template handlebars --}}
+            <!-- Appendo il contenuto del template handlebars -->
             </div>
-            <!-- Card package -->
         </div>
-
     </div>
 
+    <!-- script handlebars -->
     <script id="card_template" type="text/x-handlebars-template">
         <div class="card">
             <img class="card-img" src="{{asset('storage/')}}/@{{ img_uri }}" alt="">
@@ -71,8 +69,7 @@
             </div>
             <div class="card-body">
                 <h4 class="card-title">@{{ title }}</h4>
-                {{-- <p class="card-text">Propietario: {{$flat->user->name}}</p> --}}
-                <a class="btn btn-danger stretched-link" style="position: relative;" href="{{ url('flats/details/')}}/@{{ flat_details }}">Vedi Dettagli</a>
+                <a class="btn btn-success stretched-link" style="position: relative;" href="{{ url('flats/details/')}}/@{{ flat_details }}">Vedi Dettagli</a>
 
             </div>
         </div>
