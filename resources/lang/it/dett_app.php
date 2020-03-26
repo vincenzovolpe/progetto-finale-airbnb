6ebf78cdafc0567f7530ae3e6146cabac4cdc60b
UmbertoV <umberto.ottone@gmail.com>
2020-03-26 19:01:41 +0100

    Aggiunta traduzione pagina dettagli e messaggi

---
+++ resources/lang/it/dett_app.php
@@ -0,0 +1,33 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+    'Flat_det' => "Dettagli dell'appartamento",
+    'Address' => 'Indirizzo',
+    'Num_rooms' => 'Numero di stanze ',
+    'Num_beds' => 'Numero di letti',
+    'Num_baths' => 'Numero di bagni',
+    'Service' => 'Lista dei servizi',
+
+    'Owner' => 'Sei il proprietario di questo appartamento',
+    'Contact' => 'Contatta il proprietario',
+    'Message' => 'Messaggio',
+    'Send' => 'Invio',
+    'Map' => 'Mappa dislocazione appartamento',
+    // validation
+    'Invalid_mail' => 'Inserisci una mail corretta',
+    'Invalid_num' => 'Inserisci un numero minimo di 10 caratteri',
+
+
+];
--- resources/views/upr/flats/message.blade.php
+++ resources/views/upr/flats/message.blade.php
@@ -1,7 +1,7 @@
 @extends("layouts.upr")
 @section("content")
     <div id="upr-msg" class="container my-5">
-        <h1 class="my-5">Messaggi ricevuti</h1>
+        <h1 class="my-5">{{__('messages_tras.All_messages')}}</h1>
         <div class="row">
             <div class="col-lg-8">
                 @if ($messages->count() > 0)
@@ -11,22 +11,22 @@
                                 <table class="table table-light table-bordered">
                                     <tbody>
                                         <tr class="d-flex">
-                                            <td class="col-3 text-right">Messaggio ricevuto da</td>
+                                            <td class="col-3 text-right">{{__('messages_tras.Received_messages')}}</td>
                                             <td class="col-1 text-center"><i class="far fa-envelope-open"></i></td>
                                             <td class="col-8"> <a href="mailto:{{$message->msg_email}}">{{$message->msg_email}}</a></td>
                                         </tr>
                                         <tr class="d-flex">
-                                            <td class="col-3 text-right">Appartamento</td>
+                                            <td class="col-3 text-right">{{__('messages_tras.Appartament')}}</td>
                                             <td class="col-1 text-center"><i class="fas fa-home"></i></td>
                                             <td class="col-8">{{$message->flat->title}}</td>
                                         </tr>
                                         <tr class="d-flex">
-                                            <td class="col-3 text-right">Data Ricezione</td>
+                                            <td class="col-3 text-right">{{__('messages_tras.Reception_date')}}</td>
                                             <td class="col-1 text-center"><i class="far fa-calendar-alt"></i></td>
                                             <td class="col-8">{{$message->created_at}}</td>
                                         </tr>
                                         <tr class="d-flex">
-                                            <td class="col-3 text-right">Testo del messaggio</td>
+                                            <td class="col-3 text-right">{{__('messages_tras.Text_message')}}</td>
                                             <td class="col-1 text-center"><i class="far fa-file-alt"></i></td>
                                             <td class="col-8 table-success">{{$message->text_msg}}</td>
                                         </tr>
@@ -36,7 +36,7 @@
                         </div>
                     @endforeach
                 @else
-                    <p class="my-5">Non hai ricevuto alcun messaggio</p>
+                    <p class="my-5">{{__('messages_tras.No_mess')}}</p>
                 @endif
             </div>
         </div>
