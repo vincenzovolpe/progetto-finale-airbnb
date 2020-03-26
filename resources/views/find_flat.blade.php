09939e766ca6f6a5019556e0e5ff6b8550fa1de5
UmbertoV <umberto.ottone@gmail.com>
2020-03-26 18:39:26 +0100

    Aggiunta traduzione alla pagina find_flat

--- resources/views/find_flat.blade.php
+++ resources/views/find_flat.blade.php
@@ -4,35 +4,36 @@
     <div class="container">
         <div class="row col-md-6">
             <div id="flat_search" class="card">
-                <h2 class="card-body">Ricerca avanzata</h2>
-                <form id="form_find">
+                <h2 class="card-body">{{__('search.Advanced_search')}}</h2>
+                <form id="form_find" method="POST">
+                    @csrf
                     <div class="col">
                         <div class="form-group">
                             <div class="fuzzy-find form-group">
                             </div>
                         </div>
-                        <h4 class="text-left">Filtra i risultati</h4>
+                        <h4 class="text-left">{{__('search.Filter_results')}}</h4>
                         <div class="form-row">
                             <div class="form-group col-md-4">
                                 <!-- Inserimento numero di stanze -->
-                                <label for="room_qty" class="text-md-right">Numero stanze</label>
+                                <label for="room_qty" class="text-md-right">{{__('search.Number_of_rooms')}}</label>
                                 <input id="room_qty" type="number" name="room_qty" class="form-control" max= 20>
                             </div>
                             <div class="form-group col-md-4">
                                 <!-- Inserimento numero di letti -->
-                                <label for="bed_qty" class="text-md-right">Numero Letti</label>
+                                <label for="bed_qty" class="text-md-right">{{__('search.Number_of_beds')}}</label>
                                 <input id="bed_qty" type="number" name="bed_qty" class="form-control" max= 20 >
                             </div>
                             <div class="form-group col-md-4">
                                 <!-- Inserimento distanza in km -->
-                                <label for="km_radius" class="text-md-right">Raggio di ricerca</label>
+                                <label for="km_radius" class="text-md-right">{{__('search.Search_radius')}}</label>
                                 <input id="km_radius" type="range" name="km_radius" min="20" max="100" value="20" onchange="distance.value = this.value" class="form-control">
                                 <output id="distance"></output>Km<br>
                             </div>
                         </div>
                     </div>
                     <div class="col-md-6">
-                        <h5>Servizi disponibili</h5> <br>
+                        <h5>{{__('search.Available_services')}}</h5> <br>
                         <div class="form-group form-check col-md-6">
                             <!-- Servizi Aggiuntivi -->
                             @foreach ($services as $service)
@@ -45,7 +46,7 @@
                         <input id="latNumberFind" type="text" name="lat" value="{{ $lat }}" hidden>
                         <input id="lonNumberFind" type="text" name="lon" value="{{ $lon }}" hidden>
                         <div class="form-group col-md-6">
-                            <button id="btn_find" class="btn btn-success btn-lg" type="submit" name="btn_find">Cerca</button>
+                            <button id="btn_find" class="btn btn-success btn-lg" type="submit" name="btn_find">{{__('search.Search')}}</button>
                             <br>
                         </div>
                     </div>
@@ -69,7 +70,7 @@
             </div>
             <div class="card-body">
                 <h4 class="card-title">@{{ title }}</h4>
-                <a class="btn btn-success stretched-link" style="position: relative;" href="{{ url('flats/details/')}}/@{{ flat_details }}">Vedi Dettagli</a>
+                <a class="btn btn-success stretched-link" style="position: relative;" href="{{ url('flats/details/')}}/@{{ flat_details }}">{{__('search.Details')}}</a>
 
             </div>
         </div>
