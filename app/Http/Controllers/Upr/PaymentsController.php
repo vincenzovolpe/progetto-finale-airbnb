c6cea2cb4d75852682313af61a78f583fe4e9b36
Previtali_Fabio <fabprev93@gmail.com>
2020-03-26 11:12:44 +0100

    Risoluzione problemi di rinnovo sponsorizzazione!

--- app/Http/Controllers/Upr/PaymentsController.php
+++ app/Http/Controllers/Upr/PaymentsController.php
@@ -6,6 +6,9 @@ use App\Flat;
 use App\Sponsor;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Facades\DB;
 use Braintree_Transaction;
 
 class PaymentsController extends Controller
@@ -13,32 +16,57 @@ class PaymentsController extends Controller
 
     public function index(Request $request)
     {
+        // LOGICA DI VERIFICA UTENTE (vedi show() in FlatController per commenti):
+        $logged_user = Auth::user()->id;
+        // Trovo gli appartamenti:
         $id = $request["id"];
-        $sponsor_id = $request["sponsor_id"];
-        return view('upr.payment', ["id" => $id, "sponsor_id" => $sponsor_id]);
+        $flat = Flat::find($id);
+        // Effettuo valutazione:
+        $flat_user = $flat->user->id;
+        if($logged_user == $flat_user) {
+            $id = $request["id"];
+            $sponsor_id = $request["sponsor_id"];
+            return view('upr.payment', ["id" => $id, "sponsor_id" => $sponsor_id]);
+        } else {
+            return redirect()->route('upr.flats.index');
+        }
     }
 
+    // Funzione per il salvataggio a DB della sponsorizzazione e per il pagamento:
     public function process(Request $request, $id, $sponsor_id)
     {
-        // Trovo gli appartamenti
+        // LOGICA DI VERIFICA UTENTE (vedi show() in FlatController per commenti):
+        $logged_user = Auth::user()->id;
+        // Trovo gli appartamenti:
         $flat = Flat::find($id);
-        $sponsor = Sponsor::find($sponsor_id);
-
-        $payload = $request->input('payload', false);
-        $nonce = $payload['nonce'];
-
-        $status = Braintree_Transaction::sale([
-    	'amount' => $sponsor->price,
-    	'paymentMethodNonce' => $nonce,
-    	'options' => [
-    	    'submitForSettlement' => True
-    	]
-        ]);
-
-        $flat->sponsors()->sync($sponsor);
-
-        $flat->save();
-
-        return response()->json($status);
+        // Effettuo valutazione:
+        $flat_user = $flat->user->id;
+        if($logged_user == $flat_user) {
+            $sponsor = Sponsor::find($sponsor_id);
+            // Logica di Braintree:
+            $payload = $request->input('payload', false);
+            $nonce = $payload['nonce'];
+            $status = Braintree_Transaction::sale([
+                'amount' => $sponsor->price,
+                'paymentMethodNonce' => $nonce,
+                'options' => [
+                    'submitForSettlement' => True
+                ]
+            ]);
+            // Visto che posso rinnovare sponsorizzazioni, valutiamo se esiste già una colonna a DB: ne tengo solo una!
+            $is_flat_sponsored = DB::table('flat_sponsor')->select("sponsor_id")->where("flat_id", $id)->get();
+            // Questa è una collection, di cui posso valutare il count():
+            if ($is_flat_sponsored->count()) {
+                // Se avevo già degli sponsor, ripulisco il tutto.
+                DB::table('flat_sponsor')->where('flat_id', $id)->delete();
+            }
+            // Salvo nel DB:
+            $flat->sponsors()->sync($sponsor);
+            $flat->save();
+            // Mando risposta a Braintree:
+            return response()->json($status);
+        } else {
+            return redirect()->route('upr.flats.index');
+        }
     }
 }
--- resources/views/upr/payment.blade.php
+++ resources/views/upr/payment.blade.php
@@ -1,31 +1,43 @@
 @extends("layouts.upr")
 @section("content")
-  <div class="container">
-     <div class="row">
-       <div class="col-md-8 col-md-offset-2">
-         <div id="dropin-container"></div>
-         <button id="submit-button">Request payment method</button>
-       </div>
-     </div>
-  </div>
-  <script>
-    var button = document.querySelector('#submit-button');
+<div class="container">
+    <div class="row">
+        <div class="col">
 
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-md-8 col-md-offset-2">
+            <!-- Qui verrà inserito il dropin di Braintree: -->
+            <div id="dropin-container"></div>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col">
+            <!-- Verifica il pagamento: -->
+            <button id="submit-button">Paga ora!</button>
+            <!-- Torna in index: -->
+            <a class="btn btn-info" href="{{ route('upr.flats.index') }}">Torna all'area personale!</a>
+        </div>
+    </div>
+</div>
+<script>
+    var button = document.querySelector('#submit-button');
     braintree.dropin.create({
-      authorization: "{{ Braintree_ClientToken::generate() }}",
-      container: '#dropin-container'
+        authorization: "{{ Braintree_ClientToken::generate() }}",
+        container: '#dropin-container'
     }, function (createErr, instance) {
-      button.addEventListener('click', function () {
-        instance.requestPaymentMethod(function (err, payload) {
-          $.get('{{ route('upr.payment.process', ['id' => $id, 'sponsor_id' => $sponsor_id]) }}', {payload}, function (response) {
-            if (response.success) {
-              alert('Payment successfull!');
-            } else {
-              alert('Payment failed');
-            }
-          }, 'json');
+        button.addEventListener('click', function () {
+            instance.requestPaymentMethod(function (err, payload) {
+                $.get('{{ route('upr.payment.process', ['id' => $id, 'sponsor_id' => $sponsor_id]) }}', {payload}, function (response) {
+                    if (response.success) {
+                        alert('Payment successfull!');
+                    } else {
+                        alert('Payment failed');
+                    }
+                }, 'json');
+            });
         });
-      });
     });
-  </script>
+</script>
 @endsection
