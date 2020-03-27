eb96834af3b8334a3087b562d0f2579e1dbfb69b
Aldo-cav <aldocav@live.com>
2020-03-27 08:48:16 +0100

    fix stile upr create

--- resources/views/upr/flats/create.blade.php
+++ resources/views/upr/flats/create.blade.php
@@ -8,121 +8,129 @@
             <p>{{__('create.Filling')}}</p>
         </div>
     </div>
-    <div class="row">
-        <div class="col-lg-6">
-            <form id="create" method="POST" action="{{ route('upr.flats.store') }}" enctype="multipart/form-data">
-                @csrf
-                <!-- Inserimento titolo(descrizione) -->
-                <div class="form-group row">
-                    <label for="title" class="col-sm-3 col-form-label text-md-left">{{ __('create.Description') }}</label>
-                    <div class="col-md-9">
-                        <input id="title" class="form-control @error('title') is-invalid @enderror" type="text" name="title" value="{{ old('title')}}" required>
-                        <div class="title invalid-tooltip">
-                            {{__('create.Invalid_title')}}
+    <div class="card col-lg-10 bg-light">
+        <div class="card-body">
+            <div class="row mt-3">
+                <div class="col-lg-12">
+                    <form id="create" method="POST" action="{{ route('upr.flats.store') }}" enctype="multipart/form-data">
+                        @csrf
+                        <!-- Inserimento titolo(descrizione) -->
+                        <div class="form-group row">
+                            <label for="title" class="col-md-3 col-form-label text-md-right">{{ __('create.Description') }}<i class="fas fa-home ml-3"></i></label>
+                            <div class="col-md-9">
+                                <input id="title" class="form-control @error('title') is-invalid @enderror" type="text" name="title" value="{{ old('title')}}" required>
+                                <div class="title invalid-tooltip">
+                                    {{__('create.Invalid_title')}}
+                                </div>
+                            </div>
                         </div>
-                    </div>
-                </div>
-                <!-- Inserimento numero di stanze -->
-                <div class="form-group row">
-                    <label for="room_qty" class="col-sm-3 col-form-label text-md-left"><i class="fas fa-door-open mr-3"></i> {{ __('create.Number_of_rooms') }}</label>
-                    <div class="col-sm-3">
-                        <input id="room_qty" class="form-control @error('room_qty') is-invalid @enderror" type="number" name="room_qty" value="{{ old('room_qty')}}" required>
-                        <div class="room_qty invalid-tooltip">
-                            {{__('create.Invalid_element')}}
+                        <!-- Inserimento numero di stanze -->
+                        <div class="form-group row">
+                            <label for="room_qty" class="col-md-3 col-form-label text-md-right">{{ __('create.Number_of_rooms') }}<i class="fas fa-door-open ml-3"></i></label>
+                            <div class="col-md-2">
+                                <input id="room_qty" class="form-control @error('room_qty') is-invalid @enderror" type="number" name="room_qty" value="{{ old('room_qty')}}" required>
+                                <div class="room_qty invalid-tooltip">
+                                    {{__('create.Invalid_element')}}
+                                </div>
+                            </div>
                         </div>
-                    </div>
-                </div>
-                <!-- Inserimento numero di letti -->
-                <div class="form-group row">
-                    <label for="bed_qty" class="col-sm-3 col-form-label text-md-left"><i class="fas fa-bed mr-3"></i> {{ __('create.Number_of_beds') }}</label>
-                    <div class="col-sm-3">
-                        <input id="bed_qty" class="form-control @error('bed_qty') is-invalid @enderror" type="number" name="bed_qty" value="{{ old('bed_qty')}}" required>
-                        <div class="bed_qnty invalid-tooltip">
-                            {{__('create.Invalid_element')}}
+                        <!-- Inserimento numero di letti -->
+                        <div class="form-group row">
+                            <label for="bed_qty" class="col-md-3 col-form-label text-md-right">{{ __('create.Number_of_beds') }}<i class="fas fa-bed ml-3"></i></label>
+                            <div class="col-md-2">
+                                <input id="bed_qty" class="form-control @error('bed_qty') is-invalid @enderror" type="number" name="bed_qty" value="{{ old('bed_qty')}}" required>
+                                <div class="bed_qnty invalid-tooltip">
+                                    {{__('create.Invalid_element')}}
+                                </div>
+                            </div>
                         </div>
-                    </div>
-                </div>
-                <!-- Inserimento numero di bagni -->
-                <div class="form-group row">
-                    <label for="bath_qty" class="col-sm-3 col-form-label text-md-left"><i class="fas fa-bath mr-3"></i> {{ __('create.Number_of_baths') }}</label>
-                    <div class="col-sm-3">
-                        <input id="bath_qty" class="form-control @error('bath_qty') is-invalid @enderror" type="number" name="bath_qty" value="{{ old('bath_qty')}}" required>
-                        <div class="bath_qty invalid-tooltip">
-                            {{__('create.Invalid_element')}}
+                        <!-- Inserimento numero di bagni -->
+                        <div class="form-group row">
+                            <label for="bath_qty" class="col-md-3 col-form-label text-md-right">{{ __('create.Number_of_baths') }}<i class="fas fa-bath ml-3"></i></label>
+                            <div class="col-md-2">
+                                <input id="bath_qty" class="form-control @error('bath_qty') is-invalid @enderror" type="number" name="bath_qty" value="{{ old('bath_qty')}}" required>
+                                <div class="bath_qty invalid-tooltip">
+                                    {{__('create.Invalid_element')}}
+                                </div>
+                            </div>
                         </div>
-                    </div>
-                </div>
-                <!-- Inserimento metri quadri -->
-                <div class="form-group row">
-                    <label for="sq_meters" class="col-sm-3 col-form-label text-md-right">{{ __('create.Square_meters') }} <i class="fas fa-ruler ml-2"></i></label>
-                    <div class="col-sm-3">
-                        <input id="sq_meters" class="form-control @error('sq_meters') is-invalid @enderror" type="number" name="sq_meters" value="{{ old('sq_meters')}}" required>
-                        <div class="sq_meters invalid-tooltip">
-                            {{__('create.Invalid_mq')}}
+                        <!-- Inserimento metri quadri -->
+                        <div class="form-group row">
+                            <label for="sq_meters" class="col-md-3 col-form-label text-md-right">{{ __('create.Square_meters') }} <i class="fas fa-ruler ml-3"></i></label>
+                            <div class="col-md-2">
+                                <input id="sq_meters" class="form-control @error('sq_meters') is-invalid @enderror" type="number" name="sq_meters" value="{{ old('sq_meters')}}" required>
+                                <div class="sq_meters invalid-tooltip">
+                                    {{__('create.Invalid_mq')}}
+                                </div>
+                            </div>
                         </div>
-                    </div>
-                </div>
 
-                <!-- searchbox indirizzo collegato a tomtom -->
-                <div class="form-group row">
-                    <label class="col-sm-3 col-form-label text-md-left tt-address-label">{{ __('create.Address') }}</label>
-                    <div class="col-sm-9">
-                        {{-- <input id="address" type="text" name="address" value="{{ $flat->address }}" required hidden> --}}
-                        <div id="address-edit" class="fuzzy-create">
+                        <!-- searchbox indirizzo collegato a tomtom -->
+                        <div class="form-group row">
+                            <label class="col-md-3 col-form-label text-md-right tt-address-label">{{ __('create.Address') }}<i class="fas fa-map-marker-alt ml-3"></i></label>
+                            <div class="col-md-9">
+                                {{-- <input id="address" type="text" name="address" value="{{ $flat->address }}" required hidden> --}}
+                                <div id="address-edit" class="fuzzy-create">
+                                </div>
+                            </div>
                         </div>
-                    </div>
-                </div>
 
-                <!-- Inserimento latitudine recuperato da API tomtom -->
-                <input id="lat" type="text" name="lat" hidden>
-                <!-- Inserimento longitudine recuperato da API tomtom -->
-                <input id="lon" type="text" name="lon" hidden>
+                        <!-- campi nascosti per Inserimento latitudine recuperato da API tomtom -->
+                        <input id="lat" type="text" name="lat" hidden>
+                        <!-- campi nascosti per Inserimento longitudine recuperato da API tomtom -->
+                        <input id="lon" type="text" name="lon" hidden>
 
-                <!-- Inserimento active -->
+                        <!-- Inserimento active -->
 
-                <!-- Inserimento active -->
-                <div class="form-group row">
-                    <div class="col-sm-12">
-                        <div class="form-check form-check-inline">
-                            <input class="form-check-input" type="radio" id="active" name="active" value="1" required>
-                            <label for="active" class="col-form-label text-md-right">{{ __('create.Show_on_site') }}</label>
+                        <!-- Inserimento active -->
+                        <div class="form-group row">
+                            <div class="col-md-9 offset-md-3">
+                                <div class="form-check form-check-inline">
+                                    <input class="form-check-input" type="radio" id="active" name="active" value="1" required>
+                                    <label for="active" class="col-form-label text-md-right">{{ __('create.Show_on_site') }}</label>
+                                </div>
+                                <div class="form-check form-check-inline">
+                                    <input class="form-check-input" type="radio" id="active" name="active" value="0" required>
+                                    <label for="active" class="col-form-label text-md-right">{{__('create.Hide_on_site')}}</label>
+                                </div>
+                            </div>
                         </div>
-                        <div class="form-check form-check-inline">
-                            <input class="form-check-input" type="radio" id="active" name="active" value="0" required>
-                            <label for="active" class="col-form-label text-md-right">{{__('create.Hide_on_site')}}</label>
+
+                        <!-- Inserimento uri immagine -->
+                        <div class="col-form-group row">
+                            <div class="col-md-9 offset-md-3">
+                                <label for="img_uri" class="form-control-label">{{__('create.Upload')}}</label>
+                                <input id="img_uri" type="file" class="form-control-file @error('img_uri') is-invalid @enderror" name="img_uri">
+                                <div class="img_uri invalid-tooltip">
+                                    {{__('create.Invalid_img')}}
+                                </div>
+                            </div>
                         </div>
-                    </div>
-                </div>
 
-                <!-- Inserimento uri immagine -->
-                <div class="col-form-group row">
-                    <div class="col-sm-12">
-                        <label for="img_uri" class="form-control-label">{{__('create.Upload')}}</label>
-                        <input id="img_uri" type="file" class="form-control-file @error('img_uri') is-invalid @enderror" name="img_uri">
-                        <div class="img_uri invalid-tooltip">
-                            {{__('create.Invalid_img')}}
+                        <!-- PARTE DEI SERVIZI: -->
+                        <div class="col-md-9 offset-md-3">
+                            <h4 class="mt-4">{{__('create.Service')}}</h4>
+                            <div class="col-form-group row">
+                                <div class="col-12">
+                                    @forelse ($servizi as $service)
+                                    <input type="checkbox" id="{{ $service -> id }}" name="{{ $service -> name }}" value="{{ $service -> id }}">
+                                    <label for="{{ $service -> id }}"><i class="{{ $service->fa_icon }} mx-3"></i> {{ $service -> name }}</label><br>
+                                    @empty
+                                    <p>{{__('create.Invalid_service')}}</p>
+                                    @endforelse
+                                </div>
+                            </div>
+                            <!-- pulsante Invio modulo -->
+                            <div class="form-group">
+                                <input id="crea" type="submit" class="crea btn btn-primary my-3" value="{{__('create.Btn_create')}}">
+                            </div>
                         </div>
-                    </div>
+                    </form>
                 </div>
+            </div>
 
-                <!-- PARTE DEI SERVIZI: -->
-                <h4 class="mt-4">{{__('create.Service')}}</h4>
-                <div class="col-form-group row">
-                    <div class="col-sm-12">
-                        @forelse ($servizi as $service)
-                            <input type="checkbox" id="{{ $service -> id }}" name="{{ $service -> name }}" value="{{ $service -> id }}">
-                            <label for="{{ $service -> id }}"><i class="{{ $service->fa_icon }} mx-2"></i> {{ $service -> name }}</label><br>
-                        @empty
-                            <p>{{__('create.Invalid_service')}}</p>
-                        @endforelse
-                    </div>
-                </div>
 
-                <!-- Invio modulo -->
-                <div class="form-group">
-                    <input id="crea" type="submit" class="crea btn btn-primary my-3" value="{{__('create.Btn_create')}}">
-                </div>
-            </form>
+
         </div>
     </div>
 </div>
