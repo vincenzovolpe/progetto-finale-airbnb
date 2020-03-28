67270f045cee76cb426fbec404e372ee03dca117
Aldo-cav <aldocav@live.com>
2020-03-28 15:13:47 +0100

    nuova 404 + partial per tag head

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -1,15 +1,15 @@
-@extends('layouts.app')
+@extends('layouts.err404')
 
 @section("page_404")
 
         <div class="container-fluid">
             <div class="row justify-content-center">
-                <section id="errore">
+                <section id="errore" class="col-sm-12 col-lg-10 mt-5">
                 	<div class="content text-center">
-                		<i class="fas fa-exclamation-triangle fa-9x mb-3"></i>
-                		<h1>404</h1>
-                		<p>{{__('404_tras.Alert')}}</p>
-                		<a class="btn btn-success mt-3" href="{{ route('home') }}">{{__('404_tras.Button')}}</a>
+                		<i class="fas fa-exclamation-triangle fa-7x my-3"></i>
+                		<h1 class="mb-3">404</h1>
+                		<h3 class="my-1">{{__('404_tras.Alert')}}</h3>
+                		<a class="btn  btn-lg btn-success my-5 shadow" href="{{ route('home') }}">{{__('404_tras.Button')}}</a>
                 	</div>
                 </section>
         	</div>
