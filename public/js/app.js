f291070ddbdd322790a7a15408cffaf6b202aa83
Previtali_Fabio <fabprev93@gmail.com>
2020-03-28 20:02:11 +0100

    About_us e fix nel Controller (pagine ora vanno in 404 se sbagliate)

--- public/js/app.js
+++ public/js/app.js
@@ -70464,6 +70464,7 @@ $(document).ready(function () {
             risultati_marker_find = data.result;
             console.log($('.tt-search-box-input').val()); //console.log(risultati_marker);
             //Chiamo la funzione che mi crea la mappa nella pagina di dettaglio
+            //$('#map').empty();
 
             createMapSearch(risultati_marker_find);
             $('#card_container').empty();
@@ -70481,6 +70482,7 @@ $(document).ready(function () {
             }
           } else {
             $('#card_container').empty();
+            $('#map').empty();
             $('#card_container').append('<p>La ricerca non ha trovato nessun appartamento!<p>');
           }
         }
@@ -70655,7 +70657,10 @@ $(document).ready(function () {
 
   validationNumber('#create #room_qty', '#create .room_qty.valid-feedback', '#create .room_qty.invalid-feedback');
   validationNumber('#create #bed_qty', '#create .bed_qnty.valid-feedback', '#create .bed_qnty.invalid-feedback');
-  validationNumber('#create #bath_qty', '#create .bath_qty.valid-feedback', '#create .bath_qty.invalid-feedback'); //Funzione di validation nella pagina del Create.blade dei mq
+  validationNumber('#create #bath_qty', '#create .bath_qty.valid-feedback', '#create .bath_qty.invalid-feedback');
+  validationNumber('#edit #room_qty', '#edit .room_qty.valid-feedback', '#edit .room_qty.invalid-feedback');
+  validationNumber('#edit #bed_qty', '#edit .bed_qnty.valid-feedback', '#edit .bed_qnty.invalid-feedback');
+  validationNumber('#edit #bath_qty', '#edit .bath_qty.valid-feedback', '#edit .bath_qty.invalid-feedback'); //Funzione di validation nella pagina del Create.blade dei mq
 
   function validationMq(parametro, valido, invalido) {
     $(parametro).keyup(function () {
@@ -70802,6 +70807,8 @@ function createMapSearch(risultati) {
   console.log(risultati);
   console.log(risultati[0].lon);
   var center = [risultati[0].lon, risultati[0].lat];
+  console.log($('#searchFind').val());
+  console.log($('.tt-search-box-input').val());
   var map = _tomtom_international_web_sdk_maps__WEBPACK_IMPORTED_MODULE_0___default.a.map({
     key: 'Y2cMr97XoBZZKKVXgUS844gofkPiZFnA',
     container: 'map',
@@ -70819,7 +70826,7 @@ function createMapSearch(risultati) {
     }); //Creazione del marker all'indirizzo dell'Appartamento
 
     var marker = new _tomtom_international_web_sdk_maps__WEBPACK_IMPORTED_MODULE_0___default.a.Marker({}).setLngLat([risultati[i].lon, risultati[i].lat]).addTo(map);
-    popup.setHTML(risultati[i].title + "<br>" + risultati[i].address + "<br>" + risultati[i].lon + " " + risultati[i].lat);
+    popup.setHTML(risultati[i].title + "<br>" + risultati[i].address);
     marker.setPopup(popup); //marker.togglePopup();
   }
 }
--- resources/views/about_us.blade.php
+++ resources/views/about_us.blade.php
@@ -1,5 +1,5 @@
 @extends('layouts.app')
-@section('content')
+@section('login-content')
 <div class="about_us my-5">
     <header class="bg-primary text-center py-5 mb-4">
         <div class="container">
@@ -9,9 +9,9 @@
     <div class="container">
         <div class="row">
             <!-- Membro del team: -->
-            <div class="col-xl-3 col-md-6 mb-4">
+            <div class="col-xl-3 col-lg-3 col-md-6 col-sm-6 col-12 mb-4">
                 <div class="card border-0 shadow">
-                    <img src="https://avatars0.githubusercontent.com/u/56113258?s=460&u=99dd8d78db12317d1421f96af5ee65d48f978aa3&v=4" class="card-img-top" alt="...">
+                    <img class="card-img" src="/assets/images/aldo.jpg" alt="bool-bnb-logo">
                     <div class="card-body text-center">
                         <h5 class="card-title mb-0">Aldo Cavenago</h5>
                         <div class="card-text text-black-50">Full Stack Web Developer</div>
@@ -19,9 +19,9 @@
                 </div>
             </div>
             <!-- Membro del team: -->
-            <div class="col-xl-3 col-md-6 mb-4">
+            <div class="col-xl-3 col-lg-3 col-md-6 col-sm-6 col-12 mb-4">
                 <div class="card border-0 shadow">
-                    <img src="https://source.unsplash.com/9UVmlIb0wJU/500x350" class="card-img-top" alt="...">
+                    <img class="card-img" src="/assets/images/bafio.jpg" alt="bool-bnb-logo">
                     <div class="card-body text-center">
                         <h5 class="card-title mb-0">Fabio Previtali</h5>
                         <div class="card-text text-black-50">Full Stack Web Developer</div>
@@ -29,9 +29,9 @@
                 </div>
             </div>
             <!-- Membro del team: -->
-            <div class="col-xl-3 col-md-6 mb-4">
+            <div class="col-xl-3 col-lg-3 col-md-6 col-sm-6 col-12 mb-4">
                 <div class="card border-0 shadow">
-                    <img src="https://source.unsplash.com/sNut2MqSmds/500x350" class="card-img-top" alt="...">
+                    <img class="card-img" src="/assets/images/umbe.jpg" alt="bool-bnb-logo">
                     <div class="card-body text-center">
                         <h5 class="card-title mb-0">Umberto Vellonio</h5>
                         <div class="card-text text-black-50">Full Stack Web Developer</div>
@@ -39,9 +39,9 @@
                 </div>
             </div>
             <!-- Membro del team: -->
-            <div class="col-xl-3 col-md-6 mb-4">
+            <div class="col-xl-3 col-lg-3 col-md-6 col-sm-6 col-12 mb-4">
                 <div class="card border-0 shadow">
-                    <img src="https://source.unsplash.com/ZI6p3i9SbVU/500x350" class="card-img-top" alt="...">
+                    <img class="card-img" src="/assets/images/vince.jpg" alt="bool-bnb-logo">
                     <div class="card-body text-center">
                         <h5 class="card-title mb-0">Vincenzo Volpe</h5>
                         <div class="card-text text-black-50">Full Stack Web Developer</div>
